1a9344669e4de89700556fecc24a7973f8bb056f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-14 03:39:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+
+  get '/name' do
+    "My name is Jhon"
+  end
+
+  get '/hometown' do
+    "My hometown is Port-au-Prince"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is beneath your beautiful"
+  end
 end
