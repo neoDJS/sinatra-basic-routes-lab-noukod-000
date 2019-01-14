7ffbdc725adac5f5c35e394905de95ddd478256b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-14 03:40:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Jhon"
+  end
+
+  get '/hometown' do
+    "My hometown is Port-au-Prince"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is beneath your beautiful"
+  end
 end
